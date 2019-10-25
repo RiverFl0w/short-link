cff9ed14c9ea178cae4dbc417a48c86d474c7391
s4mpi0l <khainguyen9209@gmail.com>
2019-10-25 23:10:55 +0700

    add-prevent-generate-url-multi-time

--- app.js
+++ app.js
@@ -6,16 +6,21 @@ const __domain = "http://localhost:" + PORT + "/";
 const nedb = require("nedb");
 const bodyParser = require("body-parser");
 const generator = require("./string-generator");
+const requestIp = require("request-ip");
+
+// const restrictMiddleWare = require("./middleware/restrict.middleware");
 
 app.use(express.static("views"));
 app.use("/public", express.static("public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(requestIp.mw());
 
+let generatedUrlUsersRecently = [];
 const db = new nedb("./database/linkDB");
 db.loadDatabase();
 
-app.get("/short-link-storage", (request, response) => {
+app.get("/get/short-link-storage", (request, response) => {
   db.find({}, (err, data) => {
     if (err) throw err;
     response.send(data);
@@ -24,39 +29,56 @@ app.get("/short-link-storage", (request, response) => {
 
 app.get("/:shortUrl", (request, response) => {
   const shortUrl = __domain + request.params.shortUrl;
-  console.log(shortUrl);
   db.find({ shortedUrl: shortUrl }, (err, data) => {
     if (err) throw err;
     response.redirect(data[0].url);
   });
 });
 
-app.post("/short-link-generator", (request, response) => {
-  const ip = request.connection.remoteAddress;
-  const userAgent = request.headers["user-agent"];
-  console.log(userAgent);
-  console.log(ip);
-  let url = request.body.link;
-  if (!url) {
-    response.send({ err: "link must be fill" });
-    return;
-  }
-  if (!/[\.\s]/.test(url)) {
-    response.send({ err: "invalid link" });
-    return;
-  }
-  if (!/(http)s?(:\/\/)/.test(url)) {
-    url = "http://" + url;
-  }
+app.post(
+  "/post/short-link-generator",
+  (request, response, next) => {
+    const ip = request.clientIp;
+    const userAgent = request.headers["user-agent"];
 
-  const shortedUrl = __domain + generator(6);
-  db.insert({ url, shortedUrl }, (err, newData) => {
-    if (err) throw err;
-  });
+    if (
+      generatedUrlUsersRecently.some(
+        userInfo => JSON.stringify(userInfo) === JSON.stringify({ ip, userAgent })
+      )
+    ) {
+      response.send({ err: "you generate url too fast" });
+      return;
+    }
 
-  response.redirect("/");
-});
+    generatedUrlUsersRecently.push({ ip, userAgent });
+    setTimeout(() => {
+      generatedUrlUsersRecently.shift();
+    }, 10 * 1000);
 
+    next();
+  },
+  (request, response) => {
+    let url = request.body.link;
+    if (!url) {
+      response.send({ err: "link must be fill" });
+      return;
+    }
+    if (!/[\.]/.test(url) || /[\s\\<>{}]/.test(url)) {
+      response.send({ err: "invalid link" });
+      return;
+    }
+    if (!/(http)s?(:\/\/)/.test(url)) {
+      url = "http://" + url;
+    }
+
+    const shortedUrl = __domain + generator(6);
+    db.insert({ url, shortedUrl }, (err, newData) => {
+      if (err) throw err;
+    });
+
+    response.redirect("/");
+  }
+);
 
 app.listen(PORT, (err, res) => {
   if (err) throw err;
